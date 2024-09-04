0d5e9f2e13b0ff1bbefe92db84f124ccf1ce2f5b
Fil0611 <filippobat04@hotmail.com>
2024-09-04 20:19:51 +0200

    - Added a divider in the Home Page between the social links and the presentation - Updated styles and media queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,12 @@ export default function Home() {
         <h1>Studio Ingegneria Informatica,</h1><br />
         <h1>ma sono sempre in cerca di nuove opportunità per imparare.</h1>
       </div>
-      <div className='mt-20'>
-        <SocialLink icon={'linkedin'} text={'Linkedin'} url={'https://www.linkedin.it'} />
-        <SocialLink icon={'github'} text={'GitHub'} url={'https://www.github.it'} />
+      <div className='flex flex-col w-full border-opacity-50'>
+        <div className='divider' />
+      </div>
+      <div className='grid grid-flow-col grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-5'>
+        <SocialLink icon={'linkedin'} text={'Linkedin'} url={'https://www.linkedin.com/in/filippo-battaglin-691002207/'} />
+        <SocialLink icon={'github'} text={'GitHub'} url={'https://github.com/Fil0611'} />
       </div>
     </>
   );
--- src/components/socialLink.tsx
+++ src/components/socialLink.tsx
@@ -6,6 +6,6 @@ interface SocialLinkProps {
 
 export default function SocialLink({ icon, text, url }: SocialLinkProps) {
   return (
-    <a className='social-text text-orange-300 hover:text-orange-100 ml-10 md:' href={url} target='_blank'><i className={'bi-' + icon} /> {text}</a>
+    <a className='social-text text-orange-300 hover:text-orange-100 ml-5' href={url} target='_blank'><i className={'bi-' + icon} /> {text}</a>
   );
 }
